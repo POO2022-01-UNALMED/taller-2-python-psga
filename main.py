498e4933cd2c492f36387df697702e4ddb4c2646
psga <pusugag@unal.edu.co>
2022-03-23 03:24:50 +0000

    a

---
+++ main.py
@@ -0,0 +1,51 @@
+class Asiento():
+
+    def __init__(self,color,precio,registro):
+        self.color= color
+        self.precio= precio
+        self.registro= registro
+
+    def cambiarColor(self,color):
+        if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
+            self.color=color
+
+class Auto():
+
+    def __init__(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo=modelo
+        self.precio= precio
+        self.asientos= asientos
+        self.marca= marca
+        self.motor= motor
+        self.registro= registro
+
+    def cantidadAsientos(self):
+        contador=0
+        if self.asientos != None:
+            for asiento in self.asientos:
+                if asiento != None:
+                    contador+=1
+
+        return contador
+    
+    def verificarIntegridad(self):
+        if self.registro == self.asientos[0].registro:
+            if self.motor.registro == self.registro:
+                return ("Auto asientosoriginal")
+            else:
+                return("Las piezas no son originales")
+        else:
+            return("Las piezas no son originales")
+
+class Motor():
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo= tipo
+        self.registro = registro
+
+    def cambiarRegistro(self,registro):
+        self.registro=registro
+
+    def asignarTipo(self,tipo):
+        if tipo=="electrico" or tipo=="gasolina":
+            self.tipo=tipo
